d13adecfad9587035d8a1a706e86daa66f88fb9a
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-11 15:05:23 +0200

    manca solo la regolazione del livello

--- js/main.js
+++ js/main.js
@@ -17,34 +17,50 @@
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
-
-
 // il computer deve generare numeri casuali tra 1 e 100
 let bombList = []
-
 //crea numeri casuali 
-
 do {
- bombList.push(getRndInteger(1,10));
-// se crei un numero uguale a quello presente in lista ricrea
-//     // for (let i = 0; i < bombList.length; i++) {
-//     //  bombList.includes[i] 
-
-//  }
+ bomb = (getRndInteger(1,10));
+// se crei un numero diverso a quello presente in lista inserisci
+    if(bombList.includes(bomb) === false){
+      bombList.push(bomb);
+    }    
 }while (bombList.length < 2)
 console.log(bombList)
-
-
-
 // termina fino ad avere 16 numeri(si parte con 2 numeri)
-
-
-//chiedi all'utente 100 (si parte con 8 numeri)numeri - il numero presente nella lista bombe
-let utenteChoice = 0;
-do{
-    utenteChoice = Number(prompt("inserisci un numero da 1 a 10"));
-}while (bombList.includes(utenteChoice))
+//chiedi all'utente 100 (si parte con 8 numeri)numeri
+// se la variabile bomba trovata diventa vera, termina la partita o vengono scelti tutti i numeri liberi e si vince la partita
+let bombFinded = false;
+let utenteChoice = [];
 
 // chiedere all'utente numero fino a quando non hai 100(si parte da 8) numeri diversi
-// dopo ogni inserimento si verifica la presenza del numero nella lista
-// se il numero è presente, hai perso, se assente chiedi altro numero fino ad un massimo di 8
+while( !bombFinded && utenteChoice.length < 8){
+  let utenteC;
+  do {
+    utenteC = Number(prompt("inserisci un numero da 1 a 10"));
+// dopo ogni inserimento si verifica la presenza del numero nella lista    
+    } while( isNaN(utenteC) || utenteC < 1 || utenteC > 100);
+  if( bombList.includes(utenteC)){
+    bombFinded = true;
+  } else if (utenteChoice.includes(utenteC)) {
+    alert("numero già selezionato");
+  }else {
+    utenteChoice.push(utenteC);
+  }
+}
+// se il numero è presente, hai perso,
+if(bombFinded){
+  alert(`Hai perso il tuo punteggio è ${utenteChoice.length}`)
+}else {
+  ("HAI VINTO!!")
+}
+
+// }while (utenteChoice.length < 8)
+
+console.log(utenteChoice, bombList)
+
+// (bombList.includes(utenteChoice) (utenteChoice.length < 8)
+
+
+
